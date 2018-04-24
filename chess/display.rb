d116c6c2a1dbb6bca5d414514c87076eaf426a1f
Henry Nguyen <hhnguyen92@gmail.com>
2018-04-23 17:26:26 -0700

    Add cursor methods

---
+++ chess/display.rb
@@ -0,0 +1,17 @@
+require 'colorize'
+require 'cursor.rb'
+
+
+class Display
+  def initialize(board)
+    @board = board
+    @cursor = Cursor.new([0, 0], board)
+    
+  end
+  
+  
+  
+   
+end 
+
+
